7a9e26f97d5fbe290864c1e58425f67fd55d690a
Amusenna <ahmet@visiosoft.com.tr>
2022-06-20 13:39:01 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const superheroes = require('superheroes');
+
+const mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+
+const supervillains = require('supervillains');
+
+const mySuperVillains = supervillains.random();
+
+console.log(mySuperVillains);
+
+console.log('cafer')
